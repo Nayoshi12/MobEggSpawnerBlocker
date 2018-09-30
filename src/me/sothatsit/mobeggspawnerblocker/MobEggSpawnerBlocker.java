e3e42ab2d64bfcfb9f221f00eedaf7752d2dd95f
Matt Bautista <nayoshi12@gmail.com>
2018-09-30 16:52:04 -0400

    Updated to 1.13. Checks if item type string is SPAWN_EGG rather than getting the type SPAWN_EGG because 1.13 seperated all the spawn egg into it's won different Material Type

--- src/me/sothatsit/mobeggspawnerblocker/MobEggSpawnerBlocker.java
+++ src/me/sothatsit/mobeggspawnerblocker/MobEggSpawnerBlocker.java
@@ -1,10 +1,7 @@
 package me.sothatsit.mobeggspawnerblocker;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Material;
+
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.command.Command;
@@ -17,108 +14,108 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MobEggSpawnerBlocker extends JavaPlugin implements Listener
-{
-    
+public class MobEggSpawnerBlocker extends JavaPlugin implements Listener {
+
     private String message;
     private boolean blockCreative;
-    
+
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        
+
         reloadConfiguration();
     }
-    
+
     public void reloadConfiguration() {
         this.saveDefaultConfig();
         this.reloadConfig();
-        
+
         if (!getConfig().isSet("message") || !getConfig().isString("message")) {
             getLogger().warning("\"message\" not set or invalid in config, resetting to default");
             getConfig().set("message", "&cChanging spawners using mob eggs is disabled on this server");
             saveConfig();
         }
-        
+
         if (!getConfig().isSet("block-creative") || !getConfig().isBoolean("block-creative")) {
             getLogger().warning("\"block-creative\" not set or invalid in config, resetting to default");
             getConfig().set("block-creative", false);
             saveConfig();
         }
-        
+
         this.message = getConfig().getString("message");
         this.blockCreative = getConfig().getBoolean("block-creative");
     }
-    
+
+    void print(String str) {
+        System.out.println(str);
+    }
+
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent e)
-    {
+    public void onPlayerInteract(PlayerInteractEvent e) {
         if (!blockCreative && e.getPlayer().getGameMode() == GameMode.CREATIVE)
             return;
-        
+
         if (e.getPlayer().isOp() || e.getPlayer().hasPermission("mobeggspawnerblocker.override"))
             return;
-        
+
         if (e.getAction() != Action.RIGHT_CLICK_BLOCK && e.getAction() != Action.LEFT_CLICK_BLOCK)
             return;
-        
+
         Block b = e.getClickedBlock();
-        
+
         if (b == null)
             return;
-        
-        if (b.getType() != Material.MOB_SPAWNER)
+
+        if (b.getType() != Material.SPAWNER)
             return;
-        
+
         ItemStack i = e.getItem();
-        
+
         if (i == null)
             return;
-        
-        if (i.getType() != Material.MONSTER_EGG && i.getType() != Material.MONSTER_EGGS)
+//        print(i.toString() + "     " + i.getType().toString()+"       " + );
+
+        if (!(i.getType().toString().contains("SPAWN_EGG")))
             return;
-        
+
         CreatureSpawner cs = (CreatureSpawner) b.getState();
-        
+
         final Location loc = cs.getLocation();
         final EntityType type = cs.getSpawnedType();
-        
+
         if (message != null && !message.isEmpty())
             e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
-        
+
         e.setCancelled(true);
-        
-        Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable()
-        {
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 Block b = loc.getBlock();
-                
-                if (b == null || b.getType() != Material.MOB_SPAWNER)
+
+                if (b == null || b.getType() != Material.SPAWNER)
                     return;
-                
+
                 CreatureSpawner cs = (CreatureSpawner) b.getState();
-                
+
                 cs.setSpawnedType(type);
             }
         });
     }
-    
+
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (label.equalsIgnoreCase("mobeggspawnerblocker")) {
             if (!sender.isOp() && !sender.hasPermission("mobeggspawnerblocker.reload")) {
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4Error > &cYou do not have permission to run this command."));
                 return true;
             }
-            
+
             reloadConfiguration();
             sender.sendMessage(ChatColor.GREEN + "MobEggSpawnerBlocker config reloaded");
-            
+
             return true;
         }
-        
+
         return false;
     }
 }
