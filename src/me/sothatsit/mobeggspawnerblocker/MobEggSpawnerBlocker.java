77845bf7d45f36e8f6ca855257b9269902d2443d
Matt Bautista <nayoshi12@gmail.com>
2018-12-15 01:01:10 -0500

    Added more comments

--- src/me/sothatsit/mobeggspawnerblocker/MobEggSpawnerBlocker.java
+++ src/me/sothatsit/mobeggspawnerblocker/MobEggSpawnerBlocker.java
@@ -20,7 +20,7 @@ public class MobEggSpawnerBlocker extends JavaPlugin implements Listener {
 
     private String message;
     private boolean blockCreative;
-
+    //Server is enabled register Player Interact Listener
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
@@ -29,6 +29,7 @@ public class MobEggSpawnerBlocker extends JavaPlugin implements Listener {
     }
 
     public void reloadConfiguration() {
+        //Initialize and reinitialize the config and all the loggers
         this.saveDefaultConfig();
         this.reloadConfig();
 
@@ -54,42 +55,44 @@ public class MobEggSpawnerBlocker extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
+        //Checks if the player is in creative
         if (!blockCreative && e.getPlayer().getGameMode() == GameMode.CREATIVE)
             return;
-
+        //Checks if the player is Opped and has permissions
         if (e.getPlayer().isOp() || e.getPlayer().hasPermission("mobeggspawnerblocker.override"))
             return;
-
+        //Checks if the player is destroying the monster spawner or not
         if (e.getAction() != Action.RIGHT_CLICK_BLOCK && e.getAction() != Action.LEFT_CLICK_BLOCK)
             return;
 
         Block b = e.getClickedBlock();
 
+        //Check if the clicked block is an air or null
         if (b == null)
             return;
-
+        //Check if the block is a spawner
         if (b.getType() != Material.SPAWNER)
             return;
 
         ItemStack i = e.getItem();
-
+        //check if the held item is null
         if (i == null)
             return;
-//        print(i.toString() + "     " + i.getType().toString()+"       " + );
-
+        //check if the item is a spawn egg or not
         if (!(i.getType().toString().contains("SPAWN_EGG")))
             return;
 
         CreatureSpawner cs = (CreatureSpawner) b.getState();
-
+        //Get Locations
         final Location loc = cs.getLocation();
         final EntityType type = cs.getSpawnedType();
 
         if (message != null && !message.isEmpty())
-//            e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+            //Get the message from config for full configuration
             e.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',message)));
         e.setCancelled(true);
 
+        //Get block location and cxheck if the block is a creature spawner and if it comes down to here, it will change the spawned type
         Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
             @Override
             public void run() {
@@ -106,6 +109,8 @@ public class MobEggSpawnerBlocker extends JavaPlugin implements Listener {
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        //Command: /mobeggspawnerblocker: To reload
         if (label.equalsIgnoreCase("mobeggspawnerblocker")) {
             if (!sender.isOp() && !sender.hasPermission("mobeggspawnerblocker.reload")) {
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4Error > &cYou do not have permission to run this command."));
